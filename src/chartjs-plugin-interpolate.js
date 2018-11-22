a07bf4af24eb469d7715358b11906ea01f3fb3b6
Abel Heinsbroek <mail@abelheinsbroek.nl>
2018-11-22 17:27:28 +0100

    support multiple axis

--- src/chartjs-plugin-interpolate.js
+++ src/chartjs-plugin-interpolate.js
@@ -2,97 +2,105 @@
 
 Chart.Interaction.modes['interpolate'] = function(chart, e, options) {
 
-  var items = []
+	var items = []
 
-  var xScale = chart.scales['x-axis-0'];
-  var yScale = chart.scales['y-axis-0'];
-
-  var xValue = xScale.getValueForPixel(e.x)
-
-  var datasetIndex = 0
+	for (var datasetIndex = 0; datasetIndex < chart.data.datasets.length; datasetIndex++) {
 
 
-  for(var dataset in chart.data.datasets) {
+		// check for interpolate setting
+		if (!chart.data.datasets[datasetIndex].interpolate) {
+			continue;
+		}
 
-      if(!chart.data.datasets[datasetIndex].interpolate) {
-        datasetIndex += 1
-        continue
-      }
-
-      var data = chart.data.datasets[datasetIndex].data
-
-
-      var index = data.findIndex(function(o) { return o.x >= xValue })
-
-      if(index == -1) {
-        datasetIndex += 1
-        continue
-      }
-
-      var prev = data[index-1]
-      var next = data[index]
-      
-      
-      if(prev && next) {
-        var slope = (next.y-prev.y)/(next.x-prev.x)
-        var interpolatedValue = prev.y + (xValue - prev.x) * slope
-      }
-
-      if(chart.data.datasets[datasetIndex].steppedLine && prev) {
-        interpolatedValue = prev.y
-      }
-
-      var label = _.round(interpolatedValue, 2)
-      
-
-      if(isNaN(interpolatedValue)) {
-        datasetIndex += 1
-        continue
-      }
+		var meta = chart.getDatasetMeta(datasetIndex);
+		// do not interpolate hidden charts
+		if (meta.hidden) {
+			continue;
+		}
 
 
-      var fakePoint = {
+		var xScale = chart.scales[meta.xAxisID];
+		var yScale = chart.scales[meta.yAxisID];
 
-        value: interpolatedValue,
-
-        tooltipPosition: function() {
-          return this._model
-        },
-        hasValue: function() {
-          return true
-        },
-        _model: {
-            x: e.x,
-            y: yScale.getPixelForValue(interpolatedValue)
-        },
-        _datasetIndex: datasetIndex,
-        _index: 0,
-        _xScale: {
-          getLabelForIndex: function(index, dataset) {
-            return xValue
-          }
-        },
-        _yScale: {
-          label: label,
-          getLabelForIndex: function(index, dataset) {
-            return this.label 
-          }
-        }
-      }
-
-      items.push(fakePoint)
-      datasetIndex += 1
-    }
+		var xValue = xScale.getValueForPixel(e.x);
 
 
-  // add other items
-  var xItems = Chart.Interaction.modes.x(chart, e, options)
-  for(var index in xItems) {
-    var item = xItems[index]
-    if(!chart.data.datasets[item._datasetIndex].interpolate) {
-      items.push(item)
-    }
-  }
+		var data = chart.data.datasets[datasetIndex].data;
 
-  return items 
+		var index = data.findIndex(function(o) { return o.x >= xValue; });
+
+		if (index === -1) {
+			continue;
+		}
+
+
+		// linear interpolate value
+		var prev = data[index - 1];
+		var next = data[index];
+
+		if (prev && next) {
+			var slope = (next.y - prev.y) / (next.x - prev.x);
+			var interpolatedValue = prev.y + (xValue - prev.x) * slope;
+		}
+
+		if (chart.data.datasets[datasetIndex].steppedLine && prev) {
+			interpolatedValue = prev.y;
+		}
+
+		if (isNaN(interpolatedValue)) {
+			continue;
+		}
+
+		var yPosition = yScale.getPixelForValue(interpolatedValue);
+
+		// do not interpolate values outside of the axis limits
+		if (isNaN(yPosition)) {
+			continue;
+		}
+
+		// create a 'fake' event point
+
+		var fakePoint = {
+
+			value: interpolatedValue,
+			xValue: xValue,
+
+			tooltipPosition: function() {
+				return this._model;
+			},
+			hasValue: function() {
+				return true;
+			},
+			_model: {
+				x: e.x,
+				y: yPosition
+			},
+			_datasetIndex: datasetIndex,
+			_index: items.length,
+			_xScale: {
+				getLabelForIndex: function(indx) {
+					return items[indx].xValue;
+				}
+			},
+			_yScale: {
+				getLabelForIndex: function(indx) {
+					return items[indx].value;
+				}
+			}
+		};
+
+		items.push(fakePoint);
+	}
+
+
+	// add other, not interpolated, items
+	var xItems = Chart.Interaction.modes.x(chart, e, options)
+	for (var index in xItems) {
+		var item = xItems[index]
+		if (!chart.data.datasets[item._datasetIndex].interpolate) {
+			items.push(item)
+		}
+	}
+
+	return items;
 }
